1de91b1a8582ca2f4612a3fdd81aa6e4aa9feab2
Samir <samirdiff@proton.me>
2024-03-13 23:34:46 +0530

    Removed cache

--- app.py
+++ app.py
@@ -1,25 +1,13 @@
 import requests
 from flask import Flask, render_template, request
-from flask_caching import Cache
-
-# https://flask-caching.readthedocs.io/en/latest
-config = {
-    "DEBUG": True,                # some Flask specific configs
-    "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
-    "CACHE_DEFAULT_TIMEOUT": 300
-}
 
 app = Flask(__name__,
             static_folder='web/static',
             template_folder='web/templates')
 
-app.config.from_mapping(config);
-cache = Cache(app)
-
 
 # OUR POKEMON PAGE!
 @app.route('/')
-@cache.cached()
 def pokemon_page():
   return render_template("index.html")
 
@@ -27,7 +15,6 @@ def pokemon_page():
 POKEMON_API = "https://api.pokemon.project.projectrexa.dedyn.io/pokeapi/{query}?authorization=622BEB8354BCDC1C94E1B5B414C66"
 
 @app.route("/search", methods=["GET", "POST"])
-@cache.cached()
 def search_pokemon(): 
   query = request.form.get('query') or request.args.get('query')
   if query == None or len(query) <= 0:
